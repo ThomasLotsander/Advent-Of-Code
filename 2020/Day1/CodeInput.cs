42ebfd257aa50618c54628db10425488189ba7d6
Thomas Lotsander <Thomas.Lotsander@metamatrix.se>
2020-12-03 15:09:19 +0100

    Add project files.

---
+++ 2020/Day1/CodeInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020.Day1
+{
+    public static class CodeInput
+    {
+        public static int[] GetPuzzleInput()
+        {
+            int[] puzzleInput = new[]
+            {
+                1, 2
+            };
+            return puzzleInput;
+        }
+
+        public static int[] GetTestPuzzleInput()
+        {
+            int[] puzzleInput = new[]
+            {
+                1, 2
+            };
+
+            return puzzleInput;
+        }
+    }
+}
---
+++ 2020/Day1/Puzzle2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020.Day1
+{
+    class Puzzle2
+    {
+    }
+}
